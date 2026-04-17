275f50193946c4a94d57406f052a820cca3ff64d
Luka Moleh <luka.moleh@student.um.si>
2026-04-17 19:52:03 +0200

    Removed Spring context from tests

--- bookings-service/bookings/src/test/java/si/um/feri/bookings/BookingsApplicationTests.java
+++ bookings-service/bookings/src/test/java/si/um/feri/bookings/BookingsApplicationTests.java
@@ -1,13 +1,10 @@
 package si.um.feri.bookings;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BookingsApplicationTests {
 
     @Test
-    void contextLoads() {
+    void smokeTest() {
     }
 
 }
