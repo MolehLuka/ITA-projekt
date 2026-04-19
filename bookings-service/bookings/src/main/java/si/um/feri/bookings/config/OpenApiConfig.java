d2f8742c2efa0ad3119e62004b0dcafc9da10477
Luka Moleh <luka.moleh@student.um.si>
2026-04-19 20:27:47 +0200

    Configured nginx gateway for members and bookins service

--- bookings-service/bookings/src/main/java/si/um/feri/bookings/config/OpenApiConfig.java
+++ bookings-service/bookings/src/main/java/si/um/feri/bookings/config/OpenApiConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI bookingsOpenApi() {
+    public OpenAPI bookingsOpenApi(@Value("${openapi.server-url:http://localhost:8088}") String serverUrl) {
         var scheme = new SecurityScheme()
             .type(SecurityScheme.Type.HTTP)
             .scheme("bearer")
@@ -20,6 +22,7 @@ public class OpenApiConfig {
             .name("Authorization");
 
         return new OpenAPI()
+            .addServersItem(new Server().url(serverUrl).description("API Gateway"))
             .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
             .components(new Components().addSecuritySchemes("bearerAuth", scheme));
     }
